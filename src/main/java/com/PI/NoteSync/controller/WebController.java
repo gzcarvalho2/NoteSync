c37465d3d2dbda583f67ce721d011173a96b3ecf
Gabriel Henrique <120389414+gzcarvalho2@users.noreply.github.com>
2025-12-11 10:44:17 -0300

    Update WebController.java

--- src/main/java/com/PI/NoteSync/controller/WebController.java
+++ src/main/java/com/PI/NoteSync/controller/WebController.java
@@ -13,9 +13,4 @@ public class WebController {
         return "forward:/index.html";
     }
 
-    @GetMapping("/install")
-    public String install() {
-        return "forward:/install.html";
-    }
-
 }
